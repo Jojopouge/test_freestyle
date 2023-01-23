b3dd99f25d7e5829f1d0740a1cdefd8cd838f11d
hamodia.fr@proton.me <hamodia.fr@proton.me>
2023-01-23 13:09:06 +0100

    ajout script Arnaud

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
- #tes freestyle
-
-print('Hello world')
+#!/usr/bin/python3print("Content-type: text/html")
+print("")
+def add(b):
+return b+ba=2    print("<p>",add(a),"</p>")
+for i in range (add(a))
+    print("Le chiffre : ",a)
